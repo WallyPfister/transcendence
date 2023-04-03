0701306fed92fe93575cb2b9747cefe1ca56c87e
zoovely <yon041128@naver.com>
2023-04-03 16:57:58 +0900

    [FEAT] : CRA

---
+++ React/src/App.tsx
@@ -0,0 +1,25 @@
+import { Verify } from 'crypto';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
+import Login from './Login/Login';
+import SignUp from './SignUp/SignUp';
+
+function App() {
+  return (
+  	<div className="App">
+		<BrowserRouter>
+			<Routes>
+				<Route path='/' element={<Login/>}/>
+				<Route path='/signup' element={<SignUp/>}/>
+				{/* <Route path='/verify' element={<Verify/>}/>
+				<Route path='/main' element={<Main/>}/>
+				<Route path='/rank' element={<Rank/>}/>
+				<Route path='/profile' element={<Profile/>}/>
+				<Route path='/game' element={<Game/>}/> */}
+			</Routes>
+		</BrowserRouter>
+	</div>
+  );
+}
+
+export default App;
