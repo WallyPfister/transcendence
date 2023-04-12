b39100ae4628a5d25ee3e25dc6fe8581e38d3058
Yangson Andrew Choi <yachoi@Yangsons-MacBook-Pro.local>
2023-04-12 18:27:41 +0900

    [FEAT] add Login html and css

--- React/src/Login/Login.tsx
+++ React/src/Login/Login.tsx
@@ -2,7 +2,12 @@ import React from "react";
 import './Login.css';
 
 function Login() {
-    return (<></>)
+    return (
+        <div id="log-in">
+            <div id="title">Wally Pfister's<br/>Ping - Pong </div>
+            <button id="log-in-button">log-in</button>
+        </div>
+    )
 }
 
 export default Login;
