2e26befdb3940abaf196a51052ddda31285b5a6a
Yangson Andrew Choi <yachoi@Yangsons-MacBook-Pro.local>
2023-04-18 13:19:14 +0900

    [MODIFY] Chageed test-chetbox msg

--- React/src/Main/Main.tsx
+++ React/src/Main/Main.tsx
@@ -1,59 +1,64 @@
 import React, { useState, useEffect } from 'react';
-import io from 'socket.io-client';
+import io, { Socket } from 'socket.io-client';
 
 import './Main.css';
 
 
+
 function Main() {
 
-    const socket = io('http://localhost:3001');
+  const socket: Socket = io('http://localhost:3000');
 
-    const [message, setMessage] = useState<string>('');
-    const [messages, setMessages] = useState<string[]>([]);
-    const [room, setRoom] = useState('');
-    const [username, setUsername] = useState('');
+  interface Message {
+    username: string;
+    message: string;
+  }
 
-    useEffect(() => {
-        // Join the specified room with the given username
-        socket.emit('joinRoom', { roomId: room, username });
-        
-        // Listen for new messages in the room
-        socket.on('newMessage', (data) => {
-          setMessages([...messages, data]);
-        });
+  const [message, setMessage] = useState<string>('');
+  const [messages, setMessages] = useState<Message[]>([]);
+  const [room, setRoom] = useState<string>('');
+  const [username, setUsername] = useState<string>('');
+
+  useEffect(() => {
+    // Join the specified room with the given username
+    socket.emit('joinRoom', { roomId: room, username });
     
-        // Listen for userJoined events
-        socket.on('userJoined', (data) => {
-          console.log(`${data.username} has joined the room`);
-        });
-      }, [room, username]);
-    
-      const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        socket.emit('test', { roomId: room, username, message });
-        setMessage('');
-      };
-  
-    return (
-        <div className="chat">
-        <h1>Chat test</h1>
-          <div>
-            <input type="text" value={room} onChange={(e) => setRoom(e.target.value)} placeholder="Room ID" />
-            <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Username" />
-            <button onClick={() => socket.emit('joinRoom', { roomId: room, username: username })}>Join Room</button>
-          </div>
-          <form onSubmit={handleSubmit}>
-            <input type="text" value={message} onChange={(e) => setMessage(e.target.value)} />
-            <button type="submit">Send</button>
-          </form>
-          <div className="chat-messages">
-            {messages.map((msg, index) => (
-              <div key={index}>{msg}</div>
-            ))}
-          </div>
-        </div>
-        
-      );
+    // Listen for new messages in the room
+    socket.on('newMessage', (data: Message) => {
+      setMessages([...messages, data]);
+    });
+
+    // Listen for userJoined events
+    socket.on('userJoined', (data: { username: string }) => {
+      console.log(`${data.username} has joined the room`);
+    });
+  }, [room, username, messages]);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    socket.emit('test', { roomId: room, username, message });
+    setMessage('');
+  };
+
+  return (
+    <div id="chat-box">
+      <h1>Chat App</h1>
+      <div>
+        <input type="text" value={room} onChange={(e) => setRoom(e.target.value)} placeholder="Room ID" />
+        <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Username" />
+        <button onClick={() => socket.emit('joinRoom', { roomId: room, username })}>Join Room</button>
+      </div>
+      <div style={{ height: '200px', overflowY: 'scroll' }}>
+        {messages.map((msg: Message, index: number) => (
+          <div key={index}>{msg.username}: {msg.message}</div>
+        ))}
+      </div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={message} onChange={(e) => setMessage(e.target.value)} />
+        <button type="submit">Send</button>
+      </form>
+    </div>
+  );
 }
 
 export default Main;
