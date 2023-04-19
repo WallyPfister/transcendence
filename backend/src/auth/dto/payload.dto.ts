7fa28a91c4e02030825b4364810718cf62f33c93
Soyeon Kim <soyeonkim.9683@gmail.com>
2023-04-19 15:14:48 +0900

    [FEAT] : Add tfa to payload dto

--- backend/src/auth/dto/payload.dto.ts
+++ backend/src/auth/dto/payload.dto.ts
@@ -1,4 +1,4 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsBoolean, IsNotEmpty, IsString } from 'class-validator';
 
 export class PayloadDTO {
   @IsNotEmpty()
@@ -8,4 +8,8 @@ export class PayloadDTO {
   @IsNotEmpty()
   @IsString()
   refreshToken: string;
+
+  @IsNotEmpty()
+  @IsBoolean()
+  tfa: boolean;
 }
