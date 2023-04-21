6ecc04d4b0610f1c495fc05a573bcabf49f37dee
sunghkim <az0856@naver.com>
2023-04-21 15:34:08 +0900

    [FEAT] : general channel and websocket implement

--- backend/channel/channel.controller.ts
+++ backend/channel/channel.controller.ts
@@ -1,21 +1,21 @@
-import { Controller , Get } from '@nestjs/common';
+import { Controller , Get, Logger } from '@nestjs/common';
 import { ChannelService } from './channel.service'
-import { ChatroomDto } from './channel.chatroomdto';
 import { get } from 'http';
 import { Post } from '@nestjs/common';
 import { Body } from '@nestjs/common';
+import { io } from 'socket.io-client';
 
 @Controller('channel')
 export class ChannelController {
 	constructor(private readonly channelService: ChannelService) {}
 	
-	@Get('global')
-	async create(): Promise<ChatroomDto> {
-		return this.channelService.createGlobalChatroom();
-	}
+	// @Get('global')
+	// async create(): Promise<> {
+	// 	return this.channelService.createGlobalChatroom();
+	// }
 
-	@Post('general')
-	async createGeneral(@Body() channel: ChatroomDto): Promise<ChatroomDto> {
-		return this.channelService.createChatroom(channel);
-	}
+	// @Post('general')
+	// async createGeneral(@Body() channel: ChatroomDto): Promise<ChatroomDto> {
+	// 	return this.channelService.createChatroom(channel);
+	// }
 }
