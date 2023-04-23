9e66c458aa1428088b4be93e1fc437e8db5cd537
Soyeon Kim <soyeonkim.9683@gmail.com>
2023-04-23 17:24:48 +0900

    [FIX] : Rename dto

--- backend/src/auth/dto/refreshJwtToken.dto.ts
+++ backend/src/auth/dto/refreshJwtToken.dto.ts
@@ -1,6 +1,6 @@
 import { IsBoolean, IsNotEmpty, IsString } from 'class-validator';
 
-export class refreshJwtTokenDTO {
+export class RefreshJwtTokenDTO {
   @IsNotEmpty()
   @IsString()
   userName: string;
