53cbef5c5e938cb7ba9a4bbd47b96e0daac57563
Soyeon Kim <soyeonkim.9683@gmail.com>
2023-04-24 22:53:14 +0900

    [MODIFY] : Remove JwtAccessTokenInfo

--- backend/src/utils/type.ts
+++ backend/src/utils/type.ts
@@ -2,9 +2,5 @@ export interface JwtTokenInfo {
 	accessToken: string;
 	refreshToken: string;
 	expiresIn: number; // seconds
-}
-
-export interface JwtAccessTokenInfo {
-	accessToken: string;
-	expiresIn: number; // seconds
+	tfa: boolean;
 }
