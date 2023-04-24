7be21677c3663fc124739a07fb7be1f86e33fa1b
sunghkim <az0856@naver.com>
2023-04-24 19:32:54 +0900

    [FIX] : Forced Merge 'back' Branch Due to Conflict

--- backend/src/channel/channel.service.ts
+++ backend/src/channel/channel.service.ts
@@ -5,6 +5,8 @@ import { ChatRoomListDTO } from './chatRoomDto';
 import { SubscribeMessage, WebSocketGateway, MessageBody, WebSocketServer, OnGatewayConnection, OnGatewayDisconnect, ConnectedSocket } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 import { Logger } from '@nestjs/common';
+import { noop } from 'rxjs';
+import { IoAdapter } from '@nestjs/platform-socket.io';
 
 const prisma = new PrismaClient();
 
@@ -27,8 +29,10 @@ export class ChannelService {
 		this.chatRoomList = {
 			"lobby": {
 				roomId: "lobby",
-				roomName: '로비',
+				roomName: 'lobby',
 				chiefId: null,
+				banList: [],
+				muteList: [],
 			},
 		}
 	}
@@ -41,12 +45,12 @@ export class ChannelService {
 	@SubscribeMessage('setUser')
 	async setUser(@MessageBody() data: { nickname: string }, @ConnectedSocket() socket: Socket){
 		const nickname = data.nickname;
-	
+	``
 		socket.data.roomId = "lobby";
 		socket.data.roomName = "lobby";
 		socket.data.nickname = nickname;
 		socket.join(socket.data.roomId);
-		this.server.to(socket.data.roomId).emit('newMessage', { nickname });
+		this.server.to(socket.data.roomId).emit('newMessage', { nickname, message: `${nickname}님이 입장하셨습니다.` });
 	}
 
 	@SubscribeMessage('createRoom')
@@ -71,6 +75,8 @@ export class ChannelService {
 				roomId: roomName,
 				roomName: roomName,
 				chiefId: socket.id,
+				banList: [],
+				muteList: [],
 		}
 		socket.join(roomName);
 	}
@@ -87,6 +93,11 @@ export class ChannelService {
 			})
 			return ;
 		}
+		if (noRoom.banList.includes(socket.data.nickname) == true){
+			socket.emit("newMessage", {message: "입장이 제한된 방입니다."});
+			return ;
+		}
+
 		socket.leave(socket.data.roomId);
 		socket.data.roomId = roomId;
 		socket.data.roomName = roomId;
@@ -100,15 +111,81 @@ export class ChannelService {
 	async sendMessage(@MessageBody() data: {message: string}, @ConnectedSocket() socket: Socket) {
 		const nickname = socket.data.nickname;
 		const message = data.message;
-		console.log(`roomId ==> ${socket.data.roomId}`);
-		console.log(`roomName ==> ${socket.data.roomName}`);
-		console.log(`nickname ==> ${socket.data.nickname}`);
 		this.server.to(socket.data.roomId).emit('newMessage', {nickname, message});
 	}
 
 	@SubscribeMessage("channel")
 	async channelList(){
 		this.server.emit("channelList", Object.keys(this.channelList));
+	}
+
+	// banList에 사용자 추가
+	@SubscribeMessage("ban")
+	addBanlist(@MessageBody() data : {nickname: string}, @ConnectedSocket() socket: Socket) {
+		const nickname = data.nickname;
+		const chatRoom = this.chatRoomList[socket.data.roomId];
+
+		if (this.isBanned(nickname, socket) == true){
+			socket.emit("newMessage", {message: `이미 banlist에 ${nickname}이(가) 있습니다.`});
+			return ;
+		}
+		chatRoom.banList.push(nickname);
+		this.kick(nickname, socket.data.roomId);
+		socket.emit("newMessage", {message: `banlist에서 ${nickname}을 추가했습니다.`});
+	}
+	
+	// banList에서 사용자 제거
+	@SubscribeMessage("banCancel")
+	removeBanlist(@MessageBody() data : {nickname: string}, @ConnectedSocket() socket: Socket) {
+		const {nickname} = data;
+		const chatRoom = this.chatRoomList[socket.data.roomId];
+
+		if (this.isBanned(nickname, socket) == false){
+			socket.emit("newMessage", {message: `nobody there`});
+			return ;
+		}
+
+		const index = chatRoom.banList.indexOf(nickname);
+		if (index !== -1) {
+			chatRoom.banList.splice(index, 1);
+		}
+		socket.emit("newMessage", {message: `banlist에서 ${nickname}을 제외했습니다.`})
+	}
+
+	kick(nickname: string, roomId: string) {
+		const sockets = this.server.sockets.adapter.rooms.get(roomId);
+
+		for(const socketId of sockets){
+			const target = this.server.sockets.sockets.get(socketId);
+			console.log(target.data.nickname);
+			console.log(target.data.roomId);
+			if (target.data.nickname == nickname){
+				console.log("here we go");
+				target.leave(target.data.roomId);
+				target.data.roomId = "lobby";
+				target.data.roomName = "lobby";
+				this.server.to(roomId).emit("newMessage", {message: `${nickname} 님이 추방되었습니다.`});
+				target.join("lobby");
+				return ;
+			}
+		}
+	}
+
+	// 사용자가 banList에 있는지 확인 및 관리자인지 확인
+	isBanned(nickname: string, socket: Socket): boolean {
+		const roomId = socket.data.roomId;
+		const chatRoom = this.chatRoomList[roomId];
+
+		if (!chatRoom){
+			socket.emit("newMessage", `Room ${roomId} not found`);
+			throw new Error(`Room ${roomId} not found`);
+		}
+		if (chatRoom.chiefId != socket.id){
+			socket.emit("newMessage", `Room ${roomId} admin is not you`);
+			throw new Error(`Room ${roomId} admin is not you`);
+		}
+
+		return chatRoom.banList.includes(nickname);
 	}
 }
 
