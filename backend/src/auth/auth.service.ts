384585e2d164e37b0417aafaf4544f15d2aab36f
Soyeon Kim <soyeonkim.9683@gmail.com>
2023-04-24 22:50:18 +0900

    [FEAT] : Implement sending and verifying tfa code

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,16 +1,16 @@
 import { HttpException, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
-import { PrismaService } from 'src/prisma/prisma.service';
 import { MemberRepository } from 'src/member/member.repository';
+import { MailerService } from '@nestjs-modules/mailer';
 
 @Injectable()
 export class AuthService {
 	constructor(
 		private readonly jwtService: JwtService,
 		private readonly config: ConfigService,
-		private readonly prisma: PrismaService,
 		private readonly memberRepository: MemberRepository,
+		private readonly mailer: MailerService,
 	) { }
 
 	verifyAccessToken(token: string) {
@@ -34,17 +34,10 @@ export class AuthService {
 	}
 
 	// TODO: RefreshToken의 변조 여부 검사(다른 유저의 토큰으로 바꿔치기 여부)
-
-	async saveRefreshToken(userName: string, token: string): Promise<void> {
-		await this.prisma.member.update({
-			where: {
-				name: userName,
-			},
-			data: {
-				refreshToken: token,
-			},
-		});
-	}
+	// 의문: 현재 로그인한 유저가 A라는 정보는 토큰의 payload 를 통해서 알 수 있는데,
+	// 토큰 자체가 다른 유저로 바꿔치기 되었다면 그냥 다른 유저가 로그인한거로 인식되는게 아닌지?
+	// 다른 유저의 토큰을 탈취했는지 여부는 애초에 판단 불가하고 유효/유효하지 않음만 판단 가능한게 아닌지...
+	// 그래서 토큰에 유효기간이 있는 것이라고 생각(탈취 당하더라도 금방 만료됨)
 
 	async issueAccessToken(userName: string, tfa: boolean): Promise<string> {
 		const payload = {
@@ -83,7 +76,6 @@ export class AuthService {
 		tfa: boolean
 	): Promise<string> {
 		if (!this.verifyRefreshToken(refreshToken)) throw new HttpException('Refresh token is invalid.', 401);
-		// TODO: RefreshToken 변조 여부 검사(다른 유저와 바꿔치기 여부)
 		const token = await this.issueAccessToken(userName, tfa);
 		return token;
 	}
@@ -93,5 +85,29 @@ export class AuthService {
 		await this.memberRepository.updateStatus(name, 0);
 	}
 
-	// TODO: twoFactorAuthentication() 구현
+	async sendTfaCode(name: string, email: string): Promise<boolean> {
+		// TODO: Implement generateCode()
+		const code = await this.memberRepository.generateCode(name);
+		// // TODO: Implement issueLimitedTimeToken
+		// const limitedTimeToken = this.issueLimitedTimeToken(member.intraId);
+		try {
+			this.mailer.sendMail({
+				to: email,
+				from: 'tspong42@gmail.com',
+				subject: 'Pong Two-factor Authentication Code',
+				html: 'code: [' + code + ']\n',
+			});
+			return true;
+		} catch (err) {
+			console.log('Failed to send email.');
+			return false;
+		}
+	}
+
+	async verifyTfaCode(name: string, code: string): Promise<boolean> {
+		// const info = await this.memberRepository.getTfaCode(name);
+		// if (info.tfaCode != code)
+		// 	return false;
+		return true;
+	}
 }
