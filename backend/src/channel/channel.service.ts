bb75e8c2cdbfd7fed00a3770d1abbbf1ca0af2ea
Soyeon Kim <soyeonkim.9683@gmail.com>
2023-05-10 17:47:20 +0900

    [FEAT] : Intergrate react server and backend server in a docker compose file

--- backend/src/channel/channel.service.ts
+++ backend/src/channel/channel.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 import { ChatRoomListDTO } from './chatRoomDto';
-​
+
 import {
   SubscribeMessage,
   WebSocketGateway,
@@ -13,9 +13,9 @@ import {
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 
-​
+
 const prisma = new PrismaClient();
-​
+
 @Injectable()
 @WebSocketGateway(3001, {
   // transports: ['websocket'],
@@ -29,33 +29,33 @@ export class ChannelService {
   @WebSocketServer()
   server: Server;
   chatRoomList: Record<string, ChatRoomListDTO>;
-​
+
   constructor() {
     this.chatRoomList = {
       lobby: {
         roomId: 'lobby',
         roomName: 'lobby',
         chiefId: null,
-		adminList: [],
+        adminList: [],
         banList: [],
         muteList: {},
         password: undefined,
       },
     };
   }
-​
+
   handleConnection(socket: Socket) {
     console.log(`Client connected with ID ${socket.id}`);
     console.log(`data ==> ${socket.data.nickname}`);
   }
-​
+
   @SubscribeMessage('setUser')
   async setUser(
     @MessageBody() data: { nickname: string },
     @ConnectedSocket() socket: Socket,
   ) {
     const nickname = data.nickname;
-​
+
     socket.data.roomId = 'lobby';
     socket.data.roomName = 'lobby';
     socket.data.nickname = nickname;
@@ -64,7 +64,7 @@ export class ChannelService {
     this.server.to(socket.data.roomId).emit('addUser', nickname);
     this.channelUserList(socket);
   }
-​
+
   @SubscribeMessage('createRoom')
   async createRoom(
     @MessageBody() data: { roomId: string; password: string },
@@ -81,11 +81,11 @@ export class ChannelService {
       });
       return;
     }
-​
+
     const existingRoom = Object.values(this.chatRoomList).find(
       (room) => room.roomName === roomName,
     );
-​
+
     if (existingRoom) {
       this.server.emit('errorMessage', {
         nickname: '<system>',
@@ -93,7 +93,7 @@ export class ChannelService {
       });
       return;
     }
-​
+
     if (this.leaveRoom(socket) === false) {
       this.isChief(this.chatRoomList[socket.data.roomId], socket);
     }
@@ -103,7 +103,7 @@ export class ChannelService {
       roomId: roomName,
       roomName: roomName,
       chiefId: socket.data.nickname,
-	  adminList: [socket.data.nickname],
+      adminList: [socket.data.nickname],
       banList: [],
       muteList: {},
       password: data.password,
@@ -113,7 +113,7 @@ export class ChannelService {
     this.server.to(socket.data.roomId).emit('addUser', socket.data.nickname);
     this.channelUserList(socket);
   }
-​
+
   @SubscribeMessage('joinRoom')
   async joinRoom(
     @MessageBody() data: { roomId: string },
@@ -123,7 +123,7 @@ export class ChannelService {
     const noRoom = Object.values(this.chatRoomList).find(
       (room) => room.roomId === roomId,
     );
-​
+
     console.log(`join ${data}`);
     if (noRoom == undefined) {
       this.server.emit('errorMessage', {
@@ -154,16 +154,16 @@ export class ChannelService {
     if (this.leaveRoom(socket) === false) {
       this.isChief(this.chatRoomList[socket.data.roomId], socket);
     }
-​
+
     socket.data.roomId = roomId;
     socket.data.roomName = roomId;
-​
+
     socket.join(roomId);
     socket.emit('joinRoom', { roomName: roomId });
     this.server.to(socket.data.roomId).emit('addUser', socket.data.nickname);
     this.channelUserList(socket);
   }
-​
+
   @SubscribeMessage('sendPassword')
   async joinPrivateRoom(
     @MessageBody() data: { roomId: string; password: string },
@@ -173,7 +173,7 @@ export class ChannelService {
     const noRoom = Object.values(this.chatRoomList).find(
       (room) => room.roomId === roomId,
     );
-​
+
     console.log(`privatejoin ${data}`);
     if (noRoom == undefined) {
       this.server.emit('errorMessage', {
@@ -207,16 +207,16 @@ export class ChannelService {
     if (this.leaveRoom(socket) === false) {
       this.isChief(this.chatRoomList[socket.data.roomId], socket);
     }
-​
+
     socket.data.roomId = roomId;
     socket.data.roomName = roomId;
-​
+
     socket.join(roomId);
     socket.emit('joinRoom', { roomName: roomId });
     this.server.to(socket.data.roomId).emit('addUser', socket.data.nickname);
     this.channelUserList(socket);
   }
-​
+
   @SubscribeMessage('sendMessage')
   async sendMessage(
     @MessageBody() data: { message: string },
@@ -225,7 +225,7 @@ export class ChannelService {
     const nickname = socket.data.nickname;
     const message = data.message;
     const chatRoom = this.chatRoomList[socket.data.roomId];
-​
+
     console.log('=====send=====');
     console.log(this.chatRoomList);
     console.log(socket.data.roomId);
@@ -247,45 +247,45 @@ export class ChannelService {
       .emit('newMessage', { nickname, message });
     console.log(data);
   }
-​
+
   @SubscribeMessage('chatRoomList')
   async channelList() {
     this.server.emit('channelList', Object.keys(this.chatRoomList));
   }
-​
+
   @SubscribeMessage('exitChannel')
   async exitChannel(@ConnectedSocket() socket: Socket) {
     const chatRoom = this.chatRoomList[socket.data.roomId];
-​
+
     if (this.leaveRoom(socket) === false) {
       this.isChief(this.chatRoomList[socket.data.roomId], socket);
     }
-​
+
     socket.data.roomId = 'lobby';
     socket.data.roomName = 'lobby';
     socket.join('lobby');
     socket.emit('newMessage', `채팅방을 나와 lobby로 나오셨습니다.`);
   }
-​
+
   async isChief(chatRoom: ChatRoomListDTO, socket: Socket) {
     if (chatRoom.chiefId == socket.data.nickname) {
       const sockets = this.server.sockets.adapter.rooms.get(socket.data.roomId);
-      if (sockets){
-	  	const socketId = Array.from(sockets.values())[0];
-		const user = this.server.sockets.sockets.get(socketId);
-		this.chatRoomList[socket.data.roomId].adminList.push(user.data.nickname);
-		delete this.chatRoomList[socket.data.roomId].adminList[socket.data.nickname];
-	  }
+      if (sockets) {
+        const socketId = Array.from(sockets.values())[0];
+        const user = this.server.sockets.sockets.get(socketId);
+        this.chatRoomList[socket.data.roomId].adminList.push(user.data.nickname);
+        delete this.chatRoomList[socket.data.roomId].adminList[socket.data.nickname];
+      }
     }
   }
-​
+
   @SubscribeMessage('mute')
   async mute(
     @MessageBody() data: { nickname: string },
     @ConnectedSocket() socket: Socket,
   ) {
     const chatRoom = this.chatRoomList[socket.data.roomId];
-​
+
     if (!chatRoom) {
       socket.emit('newMessage', `Room ${socket.data.roomId} not found`);
       throw new Error(`Room ${socket.data.roomId} not found`);
@@ -297,7 +297,7 @@ export class ChannelService {
     chatRoom.muteList[data.nickname] = new Date();
     socket.emit('newMessage', `${data.nickname} is mute`);
   }
-​
+
   // banList에 사용자 추가
   @SubscribeMessage('ban')
   addBanlist(
@@ -306,7 +306,7 @@ export class ChannelService {
   ) {
     const nickname = data.nickname;
     const chatRoom = this.chatRoomList[socket.data.roomId];
-​
+
     if (this.isBanned(nickname, socket) == true) {
       socket.emit('newMessage', {
         message: `이미 banlist에 ${nickname}이(가) 있습니다.`,
@@ -319,7 +319,7 @@ export class ChannelService {
       message: `banlist에서 ${nickname}을 추가했습니다.`,
     });
   }
-​
+
   // banList에서 사용자 제거
   @SubscribeMessage('banCancel')
   removeBanlist(
@@ -328,12 +328,12 @@ export class ChannelService {
   ) {
     const { nickname } = data;
     const chatRoom = this.chatRoomList[socket.data.roomId];
-​
+
     if (this.isBanned(nickname, socket) == false) {
       socket.emit('newMessage', { message: `nobody there` });
       return;
     }
-​
+
     const index = chatRoom.banList.indexOf(nickname);
     if (index !== -1) {
       chatRoom.banList.splice(index, 1);
@@ -342,7 +342,7 @@ export class ChannelService {
       message: `banlist에서 ${nickname}을 제외했습니다.`,
     });
   }
-​
+
   // 다이렉트 메세지 만들기
   @SubscribeMessage('privateMessage')
   async privateMessage(
@@ -358,23 +358,23 @@ export class ChannelService {
         },
       })
       .then((result) => result?.socket);
-​
+
     console.log(memberId); // 이거 나중에 emit으로 수정
   }
-​
+
   kick(nickname: string, roomId: string) {
     console.log('===== kick =====');
     console.log(nickname);
     console.log(roomId);
     const sockets = this.server.sockets.adapter.rooms.get(roomId);
-​
+
     for (const socketId of sockets) {
       const target = this.server.sockets.sockets.get(socketId);
       console.log(target.data.nickname);
       console.log(target.data.roomId);
       if (target.data.nickname == nickname) {
         this.leaveRoom(target);
-​
+
         target.data.roomId = 'lobby';
         target.data.roomName = 'lobby';
         this.server
@@ -385,38 +385,38 @@ export class ChannelService {
       }
     }
   }
-​
+
   // 사용자가 banList에 있는지 확인 및 관리자인지 확인
   isBanned(nickname: string, socket: Socket): boolean {
     const roomId = socket.data.roomId;
     const chatRoom = this.chatRoomList[roomId];
-​
+
     if (!chatRoom) {
       socket.emit('newMessage', `Room ${roomId} not found`);
       throw new Error(`Room ${roomId} not found`);
     }
-	if (chatRoom.adminList.find((value) => value === nickname) != undefined){
-		socket.emit('newMessage', `Room ${roomId} admin is not you`);
-      	throw new Error(`Room ${roomId} admin is not you`);
-	}
-	if (chatRoom.adminList.find((value) => value === socket.data.nickname) == undefined){
-		socket.emit('newMessage', `Room ${roomId} ban target is admin`);
-      	throw new Error(`Room ${roomId} ban target is admin`);
-	}
-		
+    if (chatRoom.adminList.find((value) => value === nickname) != undefined) {
+      socket.emit('newMessage', `Room ${roomId} admin is not you`);
+      throw new Error(`Room ${roomId} admin is not you`);
+    }
+    if (chatRoom.adminList.find((value) => value === socket.data.nickname) == undefined) {
+      socket.emit('newMessage', `Room ${roomId} ban target is admin`);
+      throw new Error(`Room ${roomId} ban target is admin`);
+    }
+
     return chatRoom.banList.includes(nickname);
   }
-​
+
   leaveRoom(socket: Socket): boolean {
     const oldRoomId = socket.data.roomId;
     socket.leave(oldRoomId);
-	// true => 내가 마지막 사람, false => 나 말고도 사람이 더 남음
+    // true => 내가 마지막 사람, false => 나 말고도 사람이 더 남음
     if (
       this.server.sockets.adapter.rooms.get(oldRoomId) !== undefined
     ) {
       const users = [];
       const sockets = this.server.sockets.adapter.rooms.get(oldRoomId);
-​
+
       for (const socketId of sockets) {
         const user = this.server.sockets.sockets.get(socketId);
         users.push(user.data.nickname);
@@ -424,17 +424,17 @@ export class ChannelService {
       this.server.to(oldRoomId).emit('userList', users);
       return false;
     }
-    if (socket.data.roomId !== 'lobby'){
-		delete this.chatRoomList[oldRoomId];
-	}
+    if (socket.data.roomId !== 'lobby') {
+      delete this.chatRoomList[oldRoomId];
+    }
     return true;
   }
-​
+
   // 채널 유저 리스트
   async channelUserList(socket: Socket) {
     const users = [];
     const sockets = this.server.sockets.adapter.rooms.get(socket.data.roomId);
-​
+
     for (const socketId of sockets) {
       const user = this.server.sockets.sockets.get(socketId);
       console.log(user.data.nickname);
@@ -442,5 +442,14 @@ export class ChannelService {
     }
     console.log(users);
     this.server.to(socket.data.roomId).emit('userList', users);
+  }
+
+  async findSocketByName(name: string): Promise<Socket> {
+    const sockets = this.server.sockets.sockets;
+    for (const [_, socket] of sockets) {
+      if (socket.data.nickname === name)
+        return socket;
+    }
+    return null;
   }
 }
