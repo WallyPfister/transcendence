0fec22a236267b08df22db1988713f4f183d44c2
Soyeon Kim <soyeonkim.9683@gmail.com>
2023-05-11 18:45:34 +0900

    [REFACTOR] : Apply host ip address instead of localhost

--- React/src/Socket/SocketContext.ts
+++ React/src/Socket/SocketContext.ts
@@ -2,6 +2,6 @@ import React from "react";
 import { io } from "socket.io-client";
 import * as dotenv from "dotenv";
 
-export const socket = io(process.env.SOCKET_PORT);
-console.log(socket);
+export const socket = io(process.env.REACT_APP_SOCKET_PORT);
+console.log(process.env.REACT_APP_SOCKET_PORT);
 export const SocketContext = React.createContext(socket);
