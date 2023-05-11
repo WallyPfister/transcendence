712c746e5fa36a3785567485db5b9d0c49ddfcf8
Soyeon Kim <soyeonkim.9683@gmail.com>
2023-05-11 18:47:36 +0900

    [FIX] : Modify origin in cors

--- backend/src/channel/channel.service.ts
+++ backend/src/channel/channel.service.ts
@@ -19,7 +19,7 @@ const prisma = new PrismaClient();
 @WebSocketGateway(3001, {
 	// transports: ['websocket'],
 	cors: {
-		origin: process.env.FRONT_PORT,
+		origin: '*',
 		methods: ['GET', 'POST'],
 		credentials: true,
 	},
