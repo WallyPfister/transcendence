13e99ba5095c04a1893079432a35d8385d6d2be0
zoovely <yon041128@naver.com>
2023-05-12 14:31:13 +0900

    [FIX] resolve #102 fix useQuery queryKey

--- React/src/Profile/Profile.tsx
+++ React/src/Profile/Profile.tsx
@@ -62,7 +62,7 @@ const getHistory = async (user: string): Promise<Array<HistoryData>> => {
 
 function Profile() {
     const userName: string = window.location.pathname.split('/')[2] || '@'; 
-    const { data: profileData, isLoading: profileLoading, isError: profileError } = useQuery<ProfileData>('profile-data', ()=>getProfileData(userName), {retry: false, staleTime: 60 * 1000});
+    const { data: profileData, isLoading: profileLoading, isError: profileError } = useQuery<ProfileData>('profile-data-' + userName, ()=>getProfileData(userName), {retry: false, staleTime: 60 * 1000});
     const { data: history, isLoading: historyLoading, isError: histotyError } = useQuery<Array<HistoryData>>('history-data', ()=>getHistory(userName), {retry: false, staleTime: 60 * 1000});
     const socket = useContext(SocketContext);
     const { showInvite, closeInvite, inviteData } = useInviteGame(socket);
