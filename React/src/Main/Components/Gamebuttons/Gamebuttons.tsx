9c6c550219b98b97755181a5b829d378f6826917
Yangson Andrew Choi <yachoi@Yangsons-MacBook-Pro.local>
2023-05-12 15:45:08 +0900

    [FIX] Refactor components, gamebutton logics

---
+++ React/src/Main/Components/Gamebuttons/Gamebuttons.tsx
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import './Gamebuttons.css';
+
+function GameButtons({ nickname }) {
+  const [casualClicked, setCasualClicked] = useState(false);
+  const [ladderClicked, setLadderClicked] = useState(false);
+  const [casualTime, setCasualTime] = useState(0);
+  const [ladderTime, setLadderTime] = useState(0);
+  const [timerInterval, setTimerInterval] = useState(null);
+
+  const navigate = useNavigate();
+
+  const handleCasualGame = () => {
+    if (!casualClicked) {
+      setCasualClicked(true);
+      startTimer(setCasualTime);
+    }
+  };
+
+  const handleLadderGame = () => {
+    if (!ladderClicked) {
+      setLadderClicked(true);
+      startTimer(setLadderTime);
+    }
+  };
+
+  const goToRanking = (user) => {
+    navigate("/rank", { state: { user } });
+  };
+
+  const formatTime = (time) => {
+    const minutes = Math.floor(time / 60).toString().padStart(2, "0");
+    const seconds = (time % 60).toString().padStart(2, "0");
+    return `${minutes}:${seconds}`;
+  };
+
+  const startTimer = (setTime) => {
+    clearInterval(timerInterval);
+
+    const startTime = Date.now();
+    setTime(0);
+
+    const interval = setInterval(() => {
+      const elapsedTime = Math.floor((Date.now() - startTime) / 1000);
+      setTime(elapsedTime);
+    }, 1000);
+
+    setTimerInterval(interval);
+  };
+
+  return (
+    <div id="game-buttons">
+      <button
+        id="casual-button"
+        onClick={handleCasualGame}
+        disabled={ladderClicked || casualClicked}
+      >
+        {casualClicked ? formatTime(casualTime) : "1 vs 1"}
+      </button>
+      <button
+        id="ladder-button"
+        onClick={handleLadderGame}
+        disabled={ladderClicked || casualClicked}
+      >
+        {ladderClicked ? formatTime(ladderTime) : "Rank"}
+      </button>
+      <button id="ranking-button" onClick={() => goToRanking(nickname)}>
+        Ranking
+      </button>
+    </div>
+  );
+}
+
+export default GameButtons;
